6ef10ec777bd91edcce80aa2f4ad5b56cfd353f2
Michael Regis <M_regis85@hotmail.com>
2023-09-17 02:31:05 -0400

    styles

--- src/Chat.js
+++ src/Chat.js
@@ -1,13 +1,41 @@
 import './Chat.css';
 import React, { useState } from 'react';
+import {AiOutlineSend} from 'react-icons/ai'
+
+
 
 function Chat()  {
   const [messages, setMessages] = useState([]);
   const [inputText, setInputText] = useState('');
+  const [borderDimensions, setBorderDimensions] = useState({width: 0, height: 0});
+
+const inputField = document.getElementById("inputField");
+const submitButton = document.getElementById("submitButton");
+
+// Add an event listener to the input field
+inputField.addEventListener("keydown", function(event) {
+  // Check if the pressed key is Enter (key code 13)
+  if (event.key === "Enter") {
+    // Prevent the default behavior of the Enter key (e.g., form submission)
+    event.preventDefault();
+
+    // Trigger a click event on the button
+    submitButton.click();
+  }
+});
 
   const handleInputChange = (e) => {
-    setInputText(e.target.value);
+    const newText = e.target.value
+    setInputText(newText);
+
+    const textInput = document.getElementsByClassName('message');
+    setBorderDimensions({
+      width: textInput.scrollWidth,
+      height: textInput.scrollHeight
+    });
   };
+
+
 
   const handleSendMessage = () => {
     if (inputText.trim() === '') return;
@@ -39,12 +67,15 @@ function Chat()  {
       </div>
       <div className="input-container">
         <input
+          className = "Input"
           type="text"
-          placeholder="Type your message..."
+          placeholder="Talk to me..."
           value={inputText}
           onChange={handleInputChange}
         />
-        <button onClick={handleSendMessage}>Send</button>
+        <button onClick={handleSendMessage}
+          className = "butt"
+        ><AiOutlineSend/></button>
       </div>
     </div>
   );
